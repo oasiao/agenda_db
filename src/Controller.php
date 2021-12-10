d40dd5f89479bd3b70e216f7d57a2ba16a7f9217
oasiao <74007996+oasiao@users.noreply.github.com>
2021-12-10 16:36:24 +0100

    fixing create() V

--- src/Controller.php
+++ src/Controller.php
@@ -25,7 +25,7 @@ class Controller
                 }
             }
 
-            $query = "INSERT INTO contacts (Name, Surname, Lastname, Phone) VALUES ('$name','$surname','$lastname','$phone');";
+            $query = "INSERT INTO oasiao_agenda_db.public.contacts (Name, Surname, Lastname, Phone) VALUES ('$name','$surname','$lastname','$phone');";
             if ($existe === false) {
                 $connection->exec($query);
                 return true;
